93ef883f140c493842918d749b5db3d69ccb20ac
Nathan De Sousa Lopez <nathandesousalopez@gmail.com>
2021-01-01 22:30:03 -0500

    Add files via upload

---
+++ SayingHello.py
@@ -0,0 +1,6 @@
+#constraints: keep the input, string concatination, and Output seperate.
+
+# ask a user what there name is
+x = input("What is your name?")
+# print a greeting using the name that the user inputs
+print("Hello, "+ x +", nice to meet you!")
