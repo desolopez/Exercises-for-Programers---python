42276a9a5ef28953dd26e827409459d1168a308f
Nathan De Sousa Lopez <nathandesousalopez@gmail.com>
2021-01-02 20:45:20 -0500

    Update SayingHello2.py

--- SayingHello2.py
+++ SayingHello2.py
@@ -1,3 +1,3 @@
 #Create a program that prompts for your name and prints a greeting using your name
-#constraints: white a new version of the program without using any variables
+#challenge: write a new version of the program without using any variables
 print("Hello,", input("what is your name?")+",","it is nice to meet you!")
